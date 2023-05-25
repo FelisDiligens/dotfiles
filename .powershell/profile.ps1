1161b0705eb10b276f5a003a870018c619f7c5da
FelisDiligens <47528453+FelisDiligens@users.noreply.github.com>
2023-05-25 14:13:16 +0200

    Added 'fallback' prompt when starship isn't installed

--- .powershell/profile.ps1
+++ .powershell/profile.ps1
@@ -3,13 +3,20 @@
 #     . "$HOME\.powershell\profile.ps1"
 # }
 
-# Ctrl+D to exit, Ctrl+L to clear screen, and more
+# Ctrl+D to exit, Ctrl+L to clear screen, and more. Like in bash <3
 Set-PSReadlineOption -EditMode Emacs
+
+# Tab completion like in fish <3
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+
+# Predictions like in fish, automatically enabled in PSReadline 2.2.6
+# Set-PSReadLineOption -PredictionSource HistoryAndPlugin  # PowerShell Core
+# Set-PSReadLineOption -PredictionSource History           # Windows PowerShell 5.1
 
 # Source scripts
 . "$HOME\.powershell\aliases.ps1"
 . "$HOME\.powershell\functions.ps1"
-. "$HOME\.powershell\starship.ps1"
+. "$HOME\.powershell\prompt.ps1"
 
 # gsudo (choco install gsudo)
 if (Test-Path "C:\tools\gsudo\Current\gsudoModule.psd1") {
---
+++ .powershell/prompt.ps1
@@ -0,0 +1,44 @@
+﻿[bool] $ISADMIN = $false
+if ($IsWindows -or ($env:OS -eq "Windows_NT")) {
+    # Check if elevated
+    $currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+    if ($currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+        $env:STARSHIP_PROMPT_ROOT = " "
+        [bool] $ISADMIN = $true
+    }
+    $SHELLNAME = "Windows"
+} elseif ($IsLinux) {
+    $SHELLNAME = "Linux"
+} elseif ($IsMacOS) {
+    $SHELLNAME = "macOS"
+}
+
+# Show pwd in window title
+function Invoke-Starship-PreCommand {
+    if ("$(Get-Item $pwd)" -eq "$HOME") {
+        $host.ui.RawUI.WindowTitle = "~"
+    } else {
+        $host.ui.RawUI.WindowTitle = (Get-Item $pwd).Name
+    }
+}
+
+function prompt {
+    $esc = [char]27
+    $reset = "$esc[0m"
+    $green = "$esc[32m"
+    $yellow = "$esc[33m"
+    $blue = "$esc[34m"
+    $magenta = "$esc[35m"
+
+    $FolderName = (Get-Item $pwd).Name
+    if ("$(Get-Item $pwd)" -eq "$HOME") {
+        $FolderName = "~"
+    }
+    $host.ui.RawUI.WindowTitle = $FolderName
+    return "`n$green$Env:UserName@$Env:ComputerName $magenta$SHELLNAME $($blue)pwsh $yellow$FolderName$reset`n" + $(If ($ISADMIN) {"# "} Else {"$ "})
+}
+
+# Starship
+if (Get-Command "starship" -errorAction SilentlyContinue) {
+    Invoke-Expression (&starship init powershell)
+}
