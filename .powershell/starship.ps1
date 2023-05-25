4c0aaddeb9c0e39d200e96156b1d2903448a554a
FelisDiligens <47528453+FelisDiligens@users.noreply.github.com>
2023-05-25 12:14:05 +0200

    Add '#' when elevated to starship prompt

--- .powershell/starship.ps1
+++ .powershell/starship.ps1
@@ -1,9 +1,17 @@
-# Show pwd in window title
+﻿# Show pwd in window title
 function Invoke-Starship-PreCommand {
     if ("$(Get-Item $pwd)" -eq "$HOME") {
         $host.ui.RawUI.WindowTitle = "~"
     } else {
         $host.ui.RawUI.WindowTitle = (Get-Item $pwd).Name
+    }
+}
+
+# Check if elevated
+if ($env:OS -eq "Windows_NT") {
+    $currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+    if ($currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+        $env:STARSHIP_PROMPT_ROOT = " "
     }
 }
 
