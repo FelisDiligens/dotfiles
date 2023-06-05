5336455bc79afcc851268fcfb499e57bd2f201b8
FelisDiligens <47528453+FelisDiligens@users.noreply.github.com>
2023-06-05 12:17:09 +0200

    Add which function to powershell

--- .powershell/functions.ps1
+++ .powershell/functions.ps1
@@ -2,3 +2,7 @@ function mcd ([string]$Path) {
     New-Item -Path $Path -ItemType Directory
     Set-Location -Path $Path
 }
+
+function which([Parameter(Mandatory)][string]$Name) {
+    return (Get-Command $Name).Path
+}
