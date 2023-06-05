446efa46e27c08d8232cc36041459f0ef06761c1
FelisDiligens <47528453+FelisDiligens@users.noreply.github.com>
2023-06-05 12:35:00 +0200

    Improve which

--- .powershell/functions.ps1
+++ .powershell/functions.ps1
@@ -3,6 +3,13 @@ function mcd ([string]$Path) {
     Set-Location -Path $Path
 }
 
-function which([Parameter(Mandatory)][string]$Name) {
-    return (Get-Command $Name).Path
+function which ([string]$Name) {
+    if ($Name -eq "") {
+        Write-Host "Usage: which COMMAND"
+        Write-Host "Simple wrapper function around Get-Command. Returns the full path of a given command."
+    } elseif ((Get-Command $Name -errorAction SilentlyContinue) -and ((Get-Command $Name).CommandType -eq "Application")) {
+        return (Get-Command $Name).Path
+    } else {
+        Write-Host "which: no $Name in ($env:PATH)" -fore red
+    }
 }
