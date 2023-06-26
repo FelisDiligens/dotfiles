54f3f86cc22d9b2a6b473ce7bb83b37b310f5ed4
FelisDiligens <47528453+FelisDiligens@users.noreply.github.com>
2023-06-26 13:15:56 +0200

    Adding CLI helper functions

---
+++ .bash/flatpak-helpers.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Flatpak helpers
+
+if [ -x "$(command -v flatpak)" ]; then
+
+    # Lists all application identifiers
+    function flatlist {
+        # I realized way too late, that you can simply declare which columns you want instead of using regex, lol
+        #flatpak list | perl -p -e "s/^[^\t]+\t([^\t]+)\t.*\$/\1/g"
+        flatpak list --columns=application
+    }
+
+    # Greps matching application identifier
+    function flatfind {
+        flatlist | grep -i -F $1
+    }
+
+    # Runs first match
+    # (I'm way too lazy to enter the entire identifier!)
+    function flatrun {
+        flatpak run "$(flatlist | grep -i -F -m 1 $1)"
+    }
+
+fi
---
+++ .bash/imagemagick-helpers.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ -x "$(command -v magick)" ] && [ ! -x "$(command -v convert)" ]; then
+    alias "convert"="magick convert"
+fi
+
+if [ -x "$(command -v convert)" ]; then
+    function make-ico {
+        if [ $1 == *.svg ]; then
+            # "-background transparent":                  Keep transparency instead of making the background white
+            # "-size 1024x1024":                          Tell ImageMagick to draw the vector image onto a 1024px wide canvas
+            # "-gravity center -extent 1024x1024":        Extend the canvas to a square
+            # "-define icon:auto-resize=256,64,48,32,16": ico files have multiple "layers", areas are multiple of 2
+            # "${1%.*}.ico":                              replace the last file extension by ".ico"
+            convert -background transparent -size 1024x1024 -gravity center -extent 1024x1024 "$1" -define icon:auto-resize=256,64,48,32,16 "${1%.*}.ico"
+        else
+            convert -background transparent -resize 1024x -gravity center -extent 1024x1024 "$1" -define icon:auto-resize=256,64,48,32,16 "${1%.*}.ico"
+        fi
+    }
+fi
