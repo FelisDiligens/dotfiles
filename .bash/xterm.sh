173cf665ea88f410bcb14a1f151b0f4072a6a315
FelisDiligens <47528453+FelisDiligens@users.noreply.github.com>
2023-07-18 19:15:18 +0200

    Add more checks to xterm.sh

--- .bash/xterm.sh
+++ .bash/xterm.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-[[ -f ~/.Xresources ]] && [ -x "$(command -v xrdb)" ] && xrdb -merge ~/.Xresources
+# Under Wayland, you may want to use ~/.Xdefaults instead.
+
+# If the file ~/.Xresources exists...
+[[ -f ~/.Xresources ]] && \
+    # and the command 'xrdb' exists...
+    [ -x "$(command -v xrdb)" ] && \
+        # and the DISPLAY environment variable is set...
+        [ -n "$DISPLAY" ] && \
+            # and the session is x11/xorg...
+            [ "$XDG_SESSION_TYPE" == "x11" ] && \
+                # then (and only then) merge .Xresources
+                xrdb -merge ~/.Xresources
